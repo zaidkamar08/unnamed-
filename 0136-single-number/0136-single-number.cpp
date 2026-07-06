ff2b27f36656a13782e7846ea3e6830ad71a785e
Md Jaaid Kamar <zaidkamar08@gmail.com>
2026-07-06 20:39:27 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.09%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>mpp;
+        int XOR=0;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-
+            XOR=XOR^nums[i];
         }
-        
-        for(auto it:mpp){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return -1;
+        return XOR;
     }
 };
